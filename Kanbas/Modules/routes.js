607d2e8d05437a109ce34ed09d9eae906ebc49af
RGChen030699 <Chen.Rich@northeastern.edu>
2024-11-23 04:49:25 -0500

    A5 Kanbas

---
+++ Kanbas/Modules/routes.js
@@ -0,0 +1,17 @@
+import * as modulesDao from "./dao.js";
+
+export default function ModuleRoutes(app) {
+  app.put("/api/modules/:moduleId", (req, res) => {
+    const { moduleId } = req.params;
+    const moduleUpdates = req.body;
+    modulesDao.updateModule(moduleId, moduleUpdates);
+    res.sendStatus(204);
+  });
+
+  app.delete("/api/modules/:moduleId", (req, res) => {
+    const { moduleId } = req.params;
+    modulesDao.deleteModule(moduleId);
+    console.log("delete module id", moduleId);
+    res.sendStatus(204);
+  });
+}
