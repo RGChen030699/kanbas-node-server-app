0e5462caaa56916c926d444daf74d48adf3a527c
RGChen030699 <Chen.Rich@northeastern.edu>
2024-12-06 18:38:36 -0500

    A6

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -1,8 +1,6 @@
 //import db from "../Database/index.js";
 import model from "./model.js";
 
-let { users } = db;
-
 export const createUser = (user) => {
   delete user._id
   return model.create(user);
